c1d64a539ab57ba2646e35e65945dd6014c1a3f6
pavan_gawande <pavan_gawande@epam.com>
2026-03-04 14:10:50 +0530

    first running app

---
+++ src/main/java/com/pet/manage/system/PetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pet.manage.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pet/manage/system/PetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.manage.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
